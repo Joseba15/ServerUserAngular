930dab9e066acaac43f86e1681a8ed66c79191d2
Joseba15 <josebadg15@gmail.com>
2022-12-05 12:28:53 +0100

    Iniciado el ejercicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { SharedModule } from './shared/shared.module';
+import { ServersModule } from './servers/servers.module';
+import { UsersModule } from './users/users.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    ServersModule,
+    UsersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
